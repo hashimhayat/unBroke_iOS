e98c6a738552087834067e372cc21d57488e8c01
shujew <shuaib.jewon@nyu.edu>
2016-11-21 15:12:25 -0500

    Initial Commit

---
+++ UnBroke/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UnBroke
+//
+//  Created by Shuaib Jewon on 11/21/16.
+//  Copyright © 2016 nyu.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UnBroke/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UnBroke
+//
+//  Created by Shuaib Jewon on 11/21/16.
+//  Copyright © 2016 nyu.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
